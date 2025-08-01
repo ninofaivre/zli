fefd50ce368449eb6e176210d43cecb571ca6604
6e696e6f <dev@6e696e6f.dev>
2025-08-01 15:27:21 +0200

    zig 0.15.0

--- build.zig
+++ build.zig
@@ -5,16 +5,16 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Optional: expose module if reused
-    _ = b.addModule("zli", .{
+    const rootModule = b.addModule("zli", .{
         .root_source_file = b.path("src/zli.zig"),
+        .target = target,
+        .optimize = optimize,
         .single_threaded = false,
     });
 
     // Test runner
     const lib_test = b.addTest(.{
-        .root_source_file = b.path("src/zli.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = rootModule,
     });
 
     const run_test = b.addRunArtifact(lib_test);
